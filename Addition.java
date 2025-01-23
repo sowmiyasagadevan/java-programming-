ec1c232b6caab034fa30ef8fff354ee5fdde978a
sowmiyasagadevan <152515033+sowmiyasagadevan@users.noreply.github.com>
2025-01-23 22:41:04 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,8 @@
+class Addition{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int result = a+b;
+        System.out.println("The addition of a and b is "+result);
+    }
+}
